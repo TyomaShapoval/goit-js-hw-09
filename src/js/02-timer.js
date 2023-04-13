bfc78e511e896fdfa2e6d48b89e313f905544609
TyomaShapoval <shapoval1044@gmail.com>
2023-04-13 23:12:36 +0300

    done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,73 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+
+
+const flatpickr = require("flatpickr");
+const inp = document.getElementById("datetime-picker");
+const btn = document.querySelector("button[data-start]");
+const dataSpan = document.querySelectorAll(".value");
+
+let currentData = null;
+
+
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+function countTime() {
+    const dateOfOne = new Date().getTime()
+    dataSpan[0].textContent = `${Math.floor((currentData - dateOfOne)/(1000*60*60*24))}`.padStart(2, "0");
+    dataSpan[1].textContent = `${Math.floor((currentData - dateOfOne)/(1000*60*60) % 24)}`.padStart(2, "0");
+    dataSpan[2].textContent = `${Math.floor((currentData - dateOfOne)/(1000*60) % 60)}`.padStart(2, "0");
+    dataSpan[3].textContent = `${Math.floor((currentData - dateOfOne)/(1000) % 60)}`.padStart(2, "0");
+    
+    timer = setInterval(() => { 
+    const dateOf = new Date().getTime()
+    if (currentData > dateOf) {
+    dataSpan[0].textContent = `${Math.floor((currentData - dateOf)/(1000*60*60*24))}`.padStart(2, "0");
+    dataSpan[1].textContent = `${Math.floor((currentData - dateOf)/(1000*60*60) % 24)}`.padStart(2, "0");
+    dataSpan[2].textContent = `${Math.floor((currentData - dateOf)/(1000*60) % 60)}`.padStart(2, "0");
+    dataSpan[3].textContent = `${Math.floor((currentData - dateOf)/(1000) % 60)}`.padStart(2, "0");
+    } else {
+      clearInterval(timer)
+    }
+    }, 1000)
+};
+
+
+
+
+btn.disabled = true;
+btn.addEventListener("click", countTime)
+
+flatpickr(inp, {
+    enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() < new Date().getTime()) {
+        Notiflix.Notify.failure("Please choose a date in the future");
+        btn.disabled = true;
+    }
+    else {
+        btn.disabled = false;
+        currentData = selectedDates[0];
+        console.log(currentData.getTime())
+    }
+  }
+});
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delay = parseInt(form.elements.delay.value);
+  const step = parseInt(form.elements.step.value);
+  const amount = parseInt(form.elements.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, delay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
+});
